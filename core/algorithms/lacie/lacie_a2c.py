529cc836b00543d1050a74da8c60b43399af22b1
lehduong <oopsxilitol@gmail.com>
2020-07-02 17:40:20 +0700

    fixed multistep cpc model + detach advantage in in lacie_a2c

--- core/algorithms/lacie/lacie_a2c.py
+++ core/algorithms/lacie/lacie_a2c.py
@@ -37,26 +37,33 @@ class LACIE_A2C(LacieAlgo):
         action_shape = rollouts.actions.size()[-1]
         num_steps, num_processes, _ = rollouts.rewards.size()
 
+        # Estimate baseline
         values, action_log_probs, dist_entropy, _ = self.actor_critic.evaluate_actions(
             rollouts.obs[:-1].view(-1, *obs_shape),
             rollouts.recurrent_hidden_states[:-1].view(
                 -1, self.actor_critic.recurrent_hidden_state_size),
             rollouts.masks[:-1].view(-1, 1),
             rollouts.actions.view(-1, action_shape))
-
         values = values.view(num_steps, num_processes, 1)
         action_log_probs = action_log_probs.view(num_steps, num_processes, 1)
 
         advantages = rollouts.returns[:-1] - values
+        # Value loss for updating Critic Net
         value_loss = advantages.pow(2).mean()
 
-        # contrastive learning density ratio
+        # LEARNING CONTRASTIVE PREDICTIVE MODEL
+        # compute contrastive loss and accuracy
         contrastive_loss, contrastive_accuracy = self.compute_contrastive_loss(
-            rollouts, advantages)
-        # learn cpc model
+            rollouts, advantages.detach())
+        contrastive_loss = contrastive_loss.item()
+        # computed weighted advantage according to its dependency with input sequences
+        # IMPORTANCE: we need to compute the weighted before learn cpc model
+        weighted_advantages = self.compute_weighted_advantages(
+            rollouts, advantages.detach())
+        # learn cpc model for n steps
         for _ in range(self.num_cpc_steps):
             cpc_loss, _ = self.compute_contrastive_loss(
-                rollouts, advantages)
+                rollouts, advantages.detach())
 
             self.cpc_optimizer.zero_grad()
             cpc_loss.backward()
@@ -68,12 +75,10 @@ class LACIE_A2C(LacieAlgo):
 
             self.cpc_optimizer.step()
 
-        # computed weighted advantage according to its dependency with input sequences
-        weighted_advantages = self.compute_weighted_advantages(
-            rollouts, advantages.detach())
+        # Action loss of Actor Net
         action_loss = -(weighted_advantages.detach() * action_log_probs).mean()
 
-        # imitation learning
+        # IMITATION LEARNING
         imitation_loss, imitation_accuracy = torch.tensor(
             0).to(rollouts.obs.device), 0
         if self.expert:
@@ -104,6 +109,6 @@ class LACIE_A2C(LacieAlgo):
             'entropy loss': dist_entropy.item(),
             'imitation loss': imitation_loss.item(),
             'imitation accuracy': imitation_accuracy,
-            'contrastive loss': contrastive_loss.item(),
+            'contrastive loss': contrastive_loss,
             'contrastive accuracy': contrastive_accuracy
         }
