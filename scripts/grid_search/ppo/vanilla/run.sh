1c7b052719384460b0bfd8f2c21b6d94b2b6d9d0
lehduong <oopsxilitol@gmail.com>
2020-07-27 12:55:09 +0700

    add grid search config for ppo vanilla

--- scripts/grid_search/ppo/vanilla/run.sh
+++ scripts/grid_search/ppo/vanilla/run.sh
@@ -3,4 +3,6 @@ bash scripts/grid_search/ppo/vanilla/config_2.sh && \
 bash scripts/grid_search/ppo/vanilla/config_3.sh && \
 bash scripts/grid_search/ppo/vanilla/config_4.sh && \
 bash scripts/grid_search/ppo/vanilla/config_5.sh && \
-bash scripts/grid_search/ppo/vanilla/config_6.sh
+bash scripts/grid_search/ppo/vanilla/config_6.sh && \
+bash scripts/grid_search/ppo/vanilla/config_7.sh && \
+bash scripts/grid_search/ppo/vanilla/config_8.sh
