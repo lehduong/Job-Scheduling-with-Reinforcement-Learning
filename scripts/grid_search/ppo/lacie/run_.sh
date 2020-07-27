a3013191a1347aa26660252f0c2d7068d5d76b81
lehduong <oopsxilitol@gmail.com>
2020-07-27 12:18:29 +0700

    add grid search config for lacie

---
+++ scripts/grid_search/ppo/lacie/run_.sh
@@ -0,0 +1,7 @@
+bash scripts/grid_search/ppo/lacie/config_1.sh && \
+bash scripts/grid_search/ppo/lacie/config_2.sh && \
+bash scripts/grid_search/ppo/lacie/config_3.sh && \
+bash scripts/grid_search/ppo/lacie/config_4.sh && \
+bash scripts/grid_search/ppo/lacie/config_5.sh && \
+bash scripts/grid_search/ppo/lacie/config_6.sh && \
+bash scripts/grid_search/ppo/lacie/config_7.sh
